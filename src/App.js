f523005c09af1e33077fed35994adf1a9007312b
srahul099 <sr9437@srmist.edu.in>
2024-03-04 21:58:18 +0530

    Allignment changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/header/Header";
 import Homepage from "./pages/homepage/Homepage";
+import ProgressBar from "react-scroll-progress-bar";
 function App() {
   return (
     <div className="App">
-      <div className="customcursor-display"></div>
+      <ProgressBar bgcolor="var(--neon-green)" duration="0.8" />
       <div className="Header">
         <Header />
       </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./footer.css";
+import Logo from "../../common/images/LOGO FINAL white png.png";
 import {
   UilInstagramAlt,
   UilYoutube,
@@ -10,35 +11,30 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer-desc">
-        <div className="footer-left">
-          <h2>Rahul.</h2>
-          <p className="footer-userdesc">
-            I'm a web developer and designer based in India. I have a very deep
-            passion to design stuff &#10084; This Website is made with the lot
-            of love and passion.
-          </p>
-          <div className="footer-text">
-            <p>Portfolio v2.0</p>
-            <p>© 2024 Rahul</p>
-          </div>
-        </div>
-        <div className="footer-socials">
-          <h2>Follow me on</h2>
-          <div className="icons">
-            <a href="">
-              <UilInstagramAlt size="40" color="#d5ff3f" />
-            </a>
+        <h2>Rahul.</h2>
+        <p className="footer-userdesc">
+          I'm a web developer and designer based in India. I have a very deep
+          passion to design stuff &#10084; This Website is made with the lot of
+          love and passion.
+        </p>
+        <div className="icons">
+          <a href="">
+            <UilInstagramAlt size="40" color="#d5ff3f" />
+          </a>
 
-            <a href="">
-              <UilLinkedin size="40" color="#d5ff3f" />
-            </a>
-            <a href="">
-              <UilGithub size="40" color="#d5ff3f" />
-            </a>
-            <a href="">
-              <UilYoutube size="40" color="#d5ff3f" />
-            </a>
-          </div>
+          <a href="">
+            <UilLinkedin size="40" color="#d5ff3f" />
+          </a>
+          <a href="">
+            <UilGithub size="40" color="#d5ff3f" />
+          </a>
+          <a href="">
+            <UilYoutube size="40" color="#d5ff3f" />
+          </a>
+        </div>
+        <div className="footer-text">
+          <p>Portfolio v2.0</p>
+          <p>© 2024 Rahul</p>
         </div>
       </div>
     </div>
