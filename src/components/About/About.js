edb2dbb87743185d5c4545202fe3330ee90bb8dd
srahul099 <sr9437@srmist.edu.in>
2024-03-16 23:48:57 +0530

    changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,32 +1,25 @@
 import React from "react";
 import "./about.css";
+import { NavLink } from "react-router-dom";
 const About = () => {
   return (
     <div className="about-container">
       <h2 className="about-heading">About</h2>
       <p className="about-desc">
-        Greetings! I'm Rahul, a third-year Computer Science student at SRM
-        Institute of Science and Engineering. My passion lies in crafting
-        user-centric experiences as a UI/UX designer and front-end developer.
-        With a keen eye for detail and a love for clean code, I specialize in
-        building high-performance websites and applications. My expertise spans
-        HTML, CSS, JavaScript, React, Node.js, Tailwind CSS, QML, and C++. From
-        concept to execution, I strive to create digital experiences that
-        seamlessly blend functionality with aesthetics. My journey isn't just
-        confined to coding. I bring a wealth of experience in graphic design and
-        agile scrum methodologies, ensuring smooth integrations between design
-        and development. Collaboration is at the heart of my approach, as I
-        believe the best results emerge from teamwork and communication. Beyond
-        the realm of technology, I find solace in creative pursuits. Photography
-        allows me to capture moments and tell stories through my lens, while
-        graphic design unleashes my creativity in the digital realm. And when I
-        need to recharge, you'll often find me exploring new music, curating
-        playlists, and immersing myself in melodies from diverse genres. So,
-        whether you're seeking a skilled developer, a creative designer, or a
-        collaborative team player, I'm here to bring your digital visions to
-        life. Let's connect and embark on an exciting journey of innovation
-        together!
+        Hi there! I'm Rahul, a third-year Computer Science student at SRM
+        Institute with a passion for crafting user-centric experiences. I
+        specialize in UI/UX design, front-end development, and graphic design,
+        with expertise in HTML, CSS, JavaScript, React, Node.js, Tailwind CSS,
+        QML, and C++. I thrive on collaboration and bring experience in agile
+        scrum methodologies. Beyond coding, I enjoy photography, graphic design,
+        and exploring music. Let's connect and bring your digital visions to
+        life!
       </p>
+      <div className="more-about">
+        <NavLink to="/about" className="more-about">
+          More..
+        </NavLink>
+      </div>
     </div>
   );
 };
