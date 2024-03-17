d7b5d391569860618ec89222de2f4d1b5f36f885
srahul099 <sr9437@srmist.edu.in>
2024-03-17 23:20:08 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/header/Header";
@@ -41,6 +42,7 @@ function App() {
         </Routes>
         <Footer />
       </Router>
+      <SpeedInsights />
     </div>
   );
 }
