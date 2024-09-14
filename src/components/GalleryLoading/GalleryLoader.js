792ba1b055a4ca45c6d0546d20c1fda7529164d4
srahul099 <sr9437@srmist.edu.in>
2024-09-15 00:42:20 +0530

    ui fix

--- src/components/GalleryLoading/GalleryLoader.js
+++ src/components/GalleryLoading/GalleryLoader.js
@@ -3,7 +3,7 @@ import { useSpring, animated } from "react-spring";
 import "./GalleryLoader.css";
 
 const GalleryLoader = ({ value }) => {
-  const [loader, setLoader] = useState(true);
+  const [gallloader, setGallLoader] = useState(true);
   const delay = value * 10;
   const { number } = useSpring({
     from: { number: 0 },
@@ -11,20 +11,22 @@ const GalleryLoader = ({ value }) => {
     delay: delay,
     config: { mass: 1, tension: 20, friction: 10 },
     onStart: () => {
-      setLoader(true);
-      document.body.classList.add("no-scroll");
+      if (value == 0) {
+        setGallLoader(true);
+      }
+      document.body.classList.add("no-scroll-gall");
     },
     onRest: () => {
       if (value >= 31) {
-        setLoader(false);
-        document.body.classList.remove("no-scroll");
+        setGallLoader(false);
+        document.body.classList.remove("no-scroll-gall");
       }
     },
   });
 
   return (
     <div>
-      <div className={` ${loader ? "load_Container" : "slideOut"}`}>
+      <div className={` ${gallloader ? "load_Container" : "slideOut"}`}>
         <p className="counter">
           <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>{" "}
           <span>%</span>
